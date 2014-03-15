1b466022cb3cce16b279055b362a67e060f34063
micmarsh <michael.marsh42@gmail.com>
2014-03-15 18:40:24 -0400

    need to change source path, too

--- project.clj
+++ project.clj
@@ -6,6 +6,5 @@
                  [ring "1.1.0-beta2"]
                  [hiccup "1.0.0-beta1"]
                  [lein-swank "1.4.4"]]
-  :source-paths ["src/clj"]
   :ring {:handler core.main/-main}
   :main core.main)
